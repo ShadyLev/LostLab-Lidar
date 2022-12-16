93d3e6df5fcc3447626c12e62d6afc00e26c1d1e
Benedykt Cieśliński <bc248637@falmouth.ac.uk>
2022-12-16 19:46:22 +0000

    Added level loader.

---
+++ Assets/Scripts/UI/LevelLoader.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] GameObject loadingScreen;
+    [SerializeField] Slider slider;
+
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            slider.value = progress;
+
+            yield return null;
+        }
+
+    }
+}
