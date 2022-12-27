6eb43432608791df1dc4147cd882af93b8a2598e
Benedykt Cieśliński <bc248637@falmouth.ac.uk>
2022-12-27 14:47:23 +0000

    Added text to exit prefab. Added bloom to main level.

--- Assets/Scripts/GameSystems/FinishGame.cs
+++ Assets/Scripts/GameSystems/FinishGame.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class FinishGame : MonoBehaviour
 {
     ArtefactManager artefactManager; // Reference to artefact manager
+    [SerializeField] GameObject cannotExitText;
 
     private void Start()
     {
@@ -21,10 +23,23 @@ public class FinishGame : MonoBehaviour
         {
             LoadEndScene();
         }
+        else
+        {
+            StartCoroutine(ShowCannotExitText());
+        }
     }
 
     void LoadEndScene()
     {
         Debug.Log("FINISHED!");
+    }
+
+    IEnumerator ShowCannotExitText()
+    {
+        cannotExitText.SetActive(true);
+
+        yield return new WaitForSeconds(1f);
+
+        cannotExitText.SetActive(false);
     }
 }
