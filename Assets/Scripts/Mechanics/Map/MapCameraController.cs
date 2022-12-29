3feadde5c17562ecb07697de1ecc7fdb5db864e5
Benedykt Cieśliński <bc248637@falmouth.ac.uk>
2022-12-29 13:59:32 +0000

    New map camera script. Rotation only rn.

--- Assets/Scripts/Mechanics/Map/MapCameraController.cs
+++ Assets/Scripts/Mechanics/Map/MapCameraController.cs
@@ -4,80 +4,75 @@ using UnityEngine;
 
 public class MapCameraController : MonoBehaviour
 {
-    [Header("References")]
-    [SerializeField] private Camera cam;
-    [SerializeField] private Transform target;
+    public Transform target;
+    public Camera mainCamera;
+    [Range(0.1f, 5f)]
+    [Tooltip("How sensitive the mouse drag to camera rotation")]
+    public float mouseRotateSpeed = 0.8f;
+    [Range(0.01f, 100)]
+    [Tooltip("How sensitive the touch drag to camera rotation")]
+    public float touchRotateSpeed = 17.5f;
+    [Tooltip("Smaller positive value means smoother rotation, 1 means no smooth apply")]
+    public float slerpValue = 0.25f;
 
-    [Header("Camera Zoom Values")]
-    [SerializeField] private float maxZoom;
-    [SerializeField] private float minZoom;
-    [SerializeField] private float deltaZoom;
-    [SerializeField] private float currentZoom;
 
-    private Vector3 previousPosition;
+    private Vector2 swipeDirection; //swipe delta vector2
+    private Quaternion cameraRot; // store the quaternion after the slerp operation
+    private float distanceBetweenCameraAndTarget;
+
+    private float minXRotAngle = -80; //min angle around x axis
+    private float maxXRotAngle = 80; // max angle around x axis
+
+    //Mouse rotation related
+    private float rotX; // around x
+    private float rotY; // around y
 
     // Start is called before the first frame update
     void Start()
     {
-        cam.transform.Translate(0, 0, -currentZoom);
+        distanceBetweenCameraAndTarget = Vector3.Distance(mainCamera.transform.position, target.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            previousPosition = cam.ScreenToViewportPoint(Input.mousePosition);
-        }
-
-        
-
         if (Input.GetMouseButton(0))
         {
-            Vector3 direciton = previousPosition - cam.ScreenToViewportPoint(Input.mousePosition);
+            rotX += -Input.GetAxis("Mouse Y") * mouseRotateSpeed; // around X
+            rotY += Input.GetAxis("Mouse X") * mouseRotateSpeed;
+        }
 
-            cam.transform.position = target.position;
-
-            cam.transform.Rotate(new Vector3(1, 0, 0), direciton.y * 180);
-            cam.transform.Rotate(new Vector3(0, 1, 0), -direciton.x * 180, Space.World);
-
-            //cam.transform.Translate(0, 0, -currentZoom);
-            ZoomMapCamera();
-
-            previousPosition = cam.ScreenToViewportPoint(Input.mousePosition);
+        if (rotX < minXRotAngle)
+        {
+            rotX = minXRotAngle;
+        }
+        else if (rotX > maxXRotAngle)
+        {
+            rotX = maxXRotAngle;
         }
     }
 
-    void ZoomMapCamera()
+    private void LateUpdate()
     {
-        // Adjust the zoom of the camera
-        if (Input.GetAxis("Mouse ScrollWheel") > 0f) // forward
+
+        Vector3 dir = new Vector3(0, 0, -distanceBetweenCameraAndTarget); //assign value to the distance between the maincamera and the target
+
+        Quaternion newQ; // value equal to the delta change of our mouse or touch position
+
+        newQ = Quaternion.Euler(rotX, rotY, 0); //We are setting the rotation around X, Y, Z axis respectively
+
+        cameraRot = Quaternion.Slerp(cameraRot, newQ, slerpValue);  //let cameraRot value gradually reach newQ which corresponds to our touch
+        mainCamera.transform.position = target.position + cameraRot * dir;
+        mainCamera.transform.LookAt(target.position);
+
+    }
+
+    public void SetCamPos()
+    {
+        if (mainCamera == null)
         {
-            if (currentZoom + deltaZoom >= maxZoom)
-            {
-                currentZoom = maxZoom;
-                //cam.transform.Translate(0, 0, -currentZoom);
-            }
-            else
-            {
-                currentZoom += deltaZoom;
-                //cam.transform.Translate(0, 0, -currentZoom);
-            }
+            mainCamera = Camera.main;
         }
-        else if (Input.GetAxis("Mouse ScrollWheel") < 0f) // backwards
-        {
-            if (currentZoom - deltaZoom <= minZoom)
-            {
-                currentZoom = minZoom;
-                //cam.transform.Translate(0, 0, currentZoom);
-            }
-            else
-            {
-                currentZoom -= deltaZoom;
-                //cam.transform.Translate(0, 0, currentZoom);
-            }
-        }
-        cam.transform.Translate(0, 0, -currentZoom);
+        mainCamera.transform.position = new Vector3(0, 0, -distanceBetweenCameraAndTarget);
     }
 }
