3d381c3efcb8c3c068f132cf5fbb3519b9c7993c
Benedykt Cieśliński <bc248637@falmouth.ac.uk>
2022-12-31 16:01:07 +0000

    Audio changes.

--- Assets/Scripts/Monsters/AIAudioController.cs
+++ Assets/Scripts/Monsters/AIAudioController.cs
@@ -11,8 +11,10 @@ public class AIAudioController : MonoBehaviour
 
     [Header("Random clips array")]
     [SerializeField] AudioClip[] randomCreepyNoises;
+    [SerializeField] bool startTimer;
     [SerializeField] float minDelayBetweenClips;
     [SerializeField] float maxDelayBetweenClips;
+    [SerializeField] float timer;
 
     [Header("Specified clips")]
     [SerializeField] AudioClip phase1DiscoveredClip;
@@ -30,9 +32,22 @@ public class AIAudioController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-     
-        
 
+        if (startTimer)
+        {
+            timer -= Time.deltaTime;
+
+            if (timer <= 0)
+                startTimer = false;
+        }
+
+    }
+
+    void StartPlayingRandomClip()
+    {
+        timer = Random.Range(minDelayBetweenClips, maxDelayBetweenClips);
+
+        PlayRandomClip(randomCreepyNoises);
     }
 
     IEnumerator PlayRandomClip(AudioClip[] random)
